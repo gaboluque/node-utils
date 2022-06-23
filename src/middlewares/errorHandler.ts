1d28c274371cbb98132edce45da0acbb204875f2
gabolque <gluquemiranda@gmail.com>
2022-06-23 09:03:32 -0500

    Updates

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,13 +1,14 @@
 import { NextFunction, Request, Response } from "express";
 import { CustomError } from "../errors/customError";
 
-export const errorHandler = (error: Error, req: Request, res: Response, next: NextFunction) => {  
+export const errorHandler = (error: Error, req: Request, res: Response, next: NextFunction) => {
 
-  if(error instanceof CustomError) {
+  if (error instanceof CustomError) {
     return res.status(error.statusCode).json({ errors: error.serializeErrors() });
   }
 
+  console.error(error);
   res.status(400).send({
-    errors: [ { message: "Something went wrong!" } ]
+    errors: [{ message: "Something went wrong!" }]
   });
 }
