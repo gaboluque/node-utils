11690ffaf38592bdbceb15c63b32a62b8151e635
gabolque <gluquemiranda@gmail.com>
2022-07-25 17:35:55 -0500

    Updates

--- src/index.ts
+++ src/index.ts
@@ -12,5 +12,6 @@ export * from "./middlewares/currentUser";
 
 export * from "./events/basePublisher";
 export * from "./events/baseListener";
+export * from "./events/subjects";
 export * from "./events/ticketCreatedEvent";
 export * from "./events/ticketUpdatedEvent";
